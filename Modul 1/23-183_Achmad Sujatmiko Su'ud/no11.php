ee26e7a72089abcf7f0ffe2fc8ff74be1814b113
chakko-183 <jadmikoa976@gmail.com>
2025-09-30 20:56:02 +0700

    Tugas-1

---
+++ Modul 1/23-183_Achmad Sujatmiko Su'ud/no11.php
@@ -0,0 +1,10 @@
+<?php
+$string = "Hello world!";
+$word = "world";  //substring yg dicari
+$position = strpos($string, $word); // Mencari posisi 'world' dalam string
+
+if ($position !== false) {
+    echo $position;
+} else {
+    echo "Kata 'world' tidak ditemukan dalam string.";
+}
---
+++ Modul 1/23-183_Achmad Sujatmiko Su'ud/no15.php
@@ -0,0 +1,9 @@
+<?php
+function familyName($fnama, $tahun)
+{
+    echo " $fnama $tahun <br>"; // Menampilkan nama keluarga dan tahun
+}
+
+familyName("hege born in", 1975);
+familyName("stale born in", 1978);
+familyName("kai jim born in", 1983);
