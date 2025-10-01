15c17e804e02c1a61a8004193e8cf9f384ebf641
ACH-Rayyan <madfahhor@gmail.com>
2025-10-01 16:41:23 +0700

    Add files via upload

---
+++ Modul 1/24-188_Ach_Rayyan/Tugas-2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+    <?php
+    // ini embedded script
+    echo "Hello World";
+    ?>
+</body>
+</html>
