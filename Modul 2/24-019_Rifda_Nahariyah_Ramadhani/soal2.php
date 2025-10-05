ff3800f79e4ff9ffaa9bd46e8f42f5df973474b1
rifda019 <rifdanh6@gmail.com>
2025-10-05 20:18:42 +0700

    Add files via upload

---
+++ Modul 2/24-019_Rifda_Nahariyah_Ramadhani/soal2.php
@@ -0,0 +1,29 @@
+<?php
+$matkul = array("PTI", "ALPRO", "DPW", "STRUKDAT", "JARKOM", "PAW", "PSBF", "RPL");
+
+foreach ($matkul as $i) {
+    switch ($i) {
+        case "PTI":
+            echo "Saya suka $i<br>";
+            break;
+        case "ALPRO":
+            echo "Saya suka $i<br>";
+            break;
+        case "DPW":
+            echo "Saya suka $i<br>";
+            break;
+        case "STRUKDAT":
+            echo "Saya suka $i<br>";
+            break;
+        case "JARKOM":
+            echo "Saya suka $i<br>";
+            break;
+        case "PAW":
+            echo "Saya suka $i<br>";
+            break;
+        default:
+            echo "Saya tidak mengambil matkul $i<br>";
+            break;
+    }
+}
+?>
