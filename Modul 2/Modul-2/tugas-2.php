984013d29eac5cc9b5893de97792527850bc2233
Muhammad Farrel fastha_114 <farrelfastha11@gmail.com>
2025-10-06 11:02:12 +0700

    Add files via upload

---
+++ Modul 2/Modul-2/tugas-2.php
@@ -0,0 +1,27 @@
+<?php 
+    $matkul = ["PTI", "ALPRO", "DPW", "STRUKDAT", "JARKOM", "PAW", "PSBF", "RPL"];
+    foreach ($matkul as $i){
+        switch ($i){
+            case "PTI":
+                echo "Saya suka $i <br>";
+                break;
+            case "ALPRO":
+                echo "Saya suka $i <br>";
+                break;
+            case "DPW":
+                echo "Saya suka $i <br>";
+                break;
+            case "STRUKDAT":
+                echo "Saya suka $i <br>";
+                break;
+            case "JARKOM":
+                echo "Saya suka $i <br>";
+                break;
+            case "PAW":
+                echo "Saya suka $i <br>";
+                break;
+            default:
+                echo "saya tidak mengambil matkul $i <br>";
+                break;
+        }
+    }
