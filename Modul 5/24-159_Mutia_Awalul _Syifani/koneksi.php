ddf34cf0500d5b8f5756348e3ce93e512da985db
Mutia221 <syyfayaaa@gmail.com>
2025-11-04 17:31:04 +0700

    Add files via upload

---
+++ Modul 5/24-159_Mutia_Awalul _Syifani/koneksi.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername ="localhost";
+$username = "root";
+$password = "";
+$database = "store";
+
+$conn = mysqli_connect($servername,$username,$password,$database);
+
+// if($conn){
+//     echo "Berhasil Terkoneksi";
+// }
+?>
