188bf20496052019c084cd2b3f8cf2a7fae45cbf
Al farabi <al.farabi.s@zohomail.com>
2025-11-18 21:35:00 +0700

    Add files via upload

---
+++ Modul 7/24-046_Mohammad_Al_Farabi_Sidqi/penjualan.sql
@@ -0,0 +1,319 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 18, 2025 at 03:34 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id`, `nama_barang`, `harga`, `stok`, `supplier_id`) VALUES
+(1, 'Buku Tulis', 5000, 100, 1),
+(2, 'Pulpen', 3000, 200, 1),
+(3, 'Penghapus', 2000, 150, 2),
+(4, 'Pensil', 2500, 120, 2),
+(7, 'rauutan', 1000, 5, 5),
+(8, 'notebook', 100000000, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id` varchar(20) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `jenis_kelamin` enum('L','P') DEFAULT NULL,
+  `telp` varchar(12) DEFAULT NULL,
+  `alamat` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`id`, `nama`, `jenis_kelamin`, `telp`, `alamat`) VALUES
+('714a5802dddef23c6f54', 'Budi', 'L', '0822222222', 'Jl. Melati No.2'),
+('beb8bb7933ac08997a22', 'Andi', 'L', '0811111111', 'Jl. Kenanga No.1'),
+('hgfdxhgtre', 'Fulan', 'L', '08333333333', 'Jl. Sana'),
+('jdhqiudqidh1o2e12', 'Fulanah', 'P', '0844444444', 'Jl. Sini'),
+('wertyjmncxdtyh', 'A', 'L', '085555555', 'Jl. mana');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id` int(11) NOT NULL,
+  `waktu_bayar` datetime NOT NULL,
+  `total` int(11) NOT NULL,
+  `metode` enum('TUNAI','TRANSFER','EDC') NOT NULL,
+  `transaksi_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `telp` varchar(12) NOT NULL,
+  `alamat` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`id`, `nama`, `telp`, `alamat`) VALUES
+(1, 'PT Sumber Makmur', '0219876543', 'Jakartaa'),
+(2, 'UD Maju Jaya', '0317654321', 'Surabaya'),
+(3, 'CV Sentosa Abadi', '0274455667', 'Yogyakarta'),
+(4, 'PT Cahaya Baru', '0229988776', 'Bandung'),
+(5, 'UD Sinar Terang', '0618877665', 'Medan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `waktu_transaksi` date NOT NULL,
+  `keterangan` text NOT NULL,
+  `total` int(11) NOT NULL,
+  `pelanggan_id` varchar(20) NOT NULL,
+  `user_id` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `waktu_transaksi`, `keterangan`, `total`, `pelanggan_id`, `user_id`) VALUES
+(1, '2025-11-01', 'Pembelian alat tulis 1', 15000, 'beb8bb7933ac08997a22', 1),
+(2, '2025-11-02', 'Pembelian alat tulis 2', 22000, 'beb8bb7933ac08997a22', 1),
+(3, '2025-11-03', 'Pembelian alat tulis 3', 10000, 'beb8bb7933ac08997a22', 1),
+(4, '2025-11-04', 'Pembelian alat tulis 4', 27000, 'beb8bb7933ac08997a22', 1),
+(5, '2025-11-01', 'Pembelian alat tulis 1', 18000, 'beb8bb7933ac08997a22', 1),
+(6, '2025-11-02', 'Pembelian alat tulis 2', 12000, 'beb8bb7933ac08997a22', 1),
+(7, '2025-11-03', 'Pembelian alat tulis 3', 25000, 'beb8bb7933ac08997a22', 1),
+(8, '2025-11-04', 'Pembelian alat tulis 4', 30000, 'beb8bb7933ac08997a22', 1),
+(9, '2025-11-05', 'Pembelian alat tulis 9', 21000, 'beb8bb7933ac08997a22', 1),
+(10, '2025-11-06', 'Pembelian alat tulis 10', 32000, 'beb8bb7933ac08997a22', 1),
+(14, '2025-11-07', 'Anuuu', 15000, 'wertyjmncxdtyh', 1),
+(15, '2025-11-08', '---', 1000, 'jdhqiudqidh1o2e12', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi_detail`
+--
+
+CREATE TABLE `transaksi_detail` (
+  `transaksi_id` int(11) NOT NULL,
+  `barang_id` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaksi_detail`
+--
+
+INSERT INTO `transaksi_detail` (`transaksi_id`, `barang_id`, `harga`, `qty`) VALUES
+(1, 2, 3000, 1),
+(2, 3, 2000, 3),
+(3, 4, 2500, 4),
+(4, 2, 3000, 1),
+(5, 2, 3000, 1),
+(6, 3, 2000, 1),
+(7, 3, 2000, 1),
+(8, 1, 5000, 4),
+(9, 4, 2500, 2),
+(10, 1, 5000, 3),
+(14, 1, 50000, 10),
+(14, 8, 1000000000, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` tinyint(2) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(35) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `hp` varchar(20) NOT NULL,
+  `level` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama`, `alamat`, `hp`, `level`) VALUES
+(1, 'admin', '1234', 'Admin Utama', 'Jl. Merdeka No.1', '081234567890', 1),
+(2, 'kasir1', '1234', 'Kasir Satu', 'Jl. Sudirman No.2', '089876543210', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `supplier_id` (`supplier_id`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `transaksi_id` (`transaksi_id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pelanggan_id` (`pelanggan_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD PRIMARY KEY (`transaksi_id`,`barang_id`),
+  ADD KEY `barang_id` (`barang_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` tinyint(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `barang`
+--
+ALTER TABLE `barang`
+  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`id`);
+
+--
+-- Constraints for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`transaksi_id`) REFERENCES `transaksi` (`id`);
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`pelanggan_id`) REFERENCES `pelanggan` (`id`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `transaksi_detail`
+--
+ALTER TABLE `transaksi_detail`
+  ADD CONSTRAINT `transaksi_detail_ibfk_1` FOREIGN KEY (`transaksi_id`) REFERENCES `transaksi` (`id`),
+  ADD CONSTRAINT `transaksi_detail_ibfk_2` FOREIGN KEY (`barang_id`) REFERENCES `barang` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
