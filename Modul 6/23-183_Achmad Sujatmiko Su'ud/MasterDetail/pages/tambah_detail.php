1085e0c32af0339e45be4df58467d8f099f050d8
chakko-183 <jadmikoa976@gmail.com>
2025-11-19 21:28:16 +0700

    change name

--- Modul 6/23-183_Achmad Sujatmiko Su'ud/MasterDetail/pages/tambah_detail.php
+++ Modul 6/23-183_Achmad Sujatmiko Su'ud/MasterDetail/pages/tambah_detail.php
@@ -1,7 +1,6 @@
 <?php include '../koneksi.php'; ?>
 <!DOCTYPE html>
 <html lang="id">
-
 <head>
   <meta charset="UTF-8">
   <title>Tambah Detail Transaksi</title>
@@ -35,7 +34,7 @@
         <?php
         $b = mysqli_query($conn, "SELECT * FROM barang");
         while ($br = mysqli_fetch_assoc($b)) {
-          echo "<option value='{$br['id']}'>{$br['nama_barang']} - Rp {$br['harga']}</option>";
+          echo "<option value='{$br['id']}'>{$br['nama_barang']} - Rp {$br['harga']} (Stok: {$br['stok']})</option>"; // Tambah info stok
         }
         ?>
       </select><br><br>
@@ -56,12 +55,34 @@
       if (mysqli_num_rows($cek) > 0) {
         echo "<script>alert('Barang sudah ditambahkan dalam transaksi ini!');</script>";
       } else {
-        $barang = mysqli_fetch_assoc(mysqli_query($conn, "SELECT harga FROM barang WHERE id='$bid'"));
-        $harga_total = $barang['harga'] * $qty;
+        // 1. Ambil data barang (harga dan stok saat ini)
+        $barang = mysqli_fetch_assoc(mysqli_query($conn, "SELECT harga, stok FROM barang WHERE id='$bid'"));
+        $harga_satuan = $barang['harga'];
+        $stok_saat_ini = $barang['stok'];
+        $harga_total_item = $harga_satuan * $qty;
 
-        mysqli_query($conn, "INSERT INTO transaksi_detail (transaksi_id, barang_id, qty, harga) VALUES ('$tid', '$bid', '$qty', '$harga_total')");
-        mysqli_query($conn, "UPDATE transaksi SET total = (SELECT SUM(harga) FROM transaksi_detail WHERE transaksi_id='$tid') WHERE id='$tid'");
-        echo "<script>alert('Detail berhasil ditambahkan!');window.location='transaksi_detail.php';</script>";
+        // 2. Cek apakah stok mencukupi
+        if ($qty > $stok_saat_ini) {
+          echo "<script>alert('Stok barang tidak mencukupi! Stok saat ini: " . $stok_saat_ini . "');</script>";
+        } else {
+          // Stok mencukupi, lanjutkan proses
+          
+          // 3. Masukkan detail transaksi
+          $insert_detail = mysqli_query($conn, "INSERT INTO transaksi_detail (transaksi_id, barang_id, qty, harga) 
+                                               VALUES ('$tid', '$bid', '$qty', '$harga_total_item')");
+
+          if ($insert_detail) {
+            // 4. Update/Kurangi Stok Barang
+            mysqli_query($conn, "UPDATE barang SET stok = stok - $qty WHERE id='$bid'");
+
+            // 5. Update Total Transaksi
+            mysqli_query($conn, "UPDATE transaksi SET total = (SELECT SUM(harga) FROM transaksi_detail WHERE transaksi_id='$tid') WHERE id='$tid'");
+
+            echo "<script>alert('Detail berhasil ditambahkan dan stok telah dikurangi!');window.location='transaksi_detail.php';</script>";
+          } else {
+            echo "<script>alert('Gagal menambahkan detail transaksi.');</script>";
+          }
+        }
       }
     }
     ?>
